7e707a00ee421949c48d364b80baa01800b734fc
favcri <favell81cristina>
2020-12-20 18:47:32 +0100

    Correction .xsd (xs:long & xs:string pour les dates) Changement dans le dossier jaxb

--- src/main/java/ch/hegarc/ig/cpo/jaxb/ImportFromXML.java
+++ src/main/java/ch/hegarc/ig/cpo/jaxb/ImportFromXML.java
@@ -1,5 +1,6 @@
 package ch.hegarc.ig.cpo.jaxb;
 
+import ch.hegarc.ig.business.Donateur;
 import ch.hegarc.ig.business.Projet;
 
 import javax.xml.bind.JAXBContext;
@@ -26,9 +27,15 @@ public class ImportFromXML {
 			Dataset projets = o.getValue ();
 
 //			Pour récupérer les projets du XML et les garder dans le programme
-			for (Dataset.Record rec : projets.getRecord ())
-				projetList.add (new Projet (rec.getId (), rec.getProjet (), rec.getDonateurs ()));
-
+			for (Dataset.Record rec : projets.getRecord ()) {
+				List<Donateur> donateurs = new LinkedList <> ();
+				for (Dataset.Record.Donateurs d : rec.getDonateurs()){
+					Donateur donateur = new Donateur (d.getId (), d.getPrenom (), d.getNom (), d.getEmail(), d.getLangue(), d.getAdresse(), d.getVille(), d.getMonnaie(), d.getSomme(), d.isPaye (), d.isAnnule (), d.getDateDon(), d.getDateVersement ());
+					donateurs.add (donateur);
+				}
+				Projet projet = new Projet (rec.getId (), rec.getProjet (), donateurs);
+				projetList.add(projet);
+			}
 			logger.log (Level.INFO, "Fichier '" + fileName + "' importé dans le programme avec succès ! ");
 		} catch (Exception ex) {
 			Logger.getLogger (ImportFromXML.class.getName ()).log (Level.SEVERE, null, ex);
